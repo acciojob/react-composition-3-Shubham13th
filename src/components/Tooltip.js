952e33ec95e425468fe44e1be07269e100f14fec
shubh <shubhamsharmajcc@gmail.com>
2026-02-11 12:47:11 +0530

    blaw blaw

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,18 +1,16 @@
 import React, { useState } from "react";
 
 const Tooltip = ({ text, children }) => {
-  const [visible, setVisible] = useState(false);
+  const [show, setShow] = useState(false);
 
-  // Clone the child element (h2, p, span, etc.)
   return React.cloneElement(children, {
     className: "tooltip",
-    onMouseEnter: () => setVisible(true),
-    onMouseLeave: () => setVisible(false),
-    children: (
-      <>
-        {children.props.children}
-        {visible && <div className="tooltiptext">{text}</div>}
-      </>
+    onMouseEnter: () => setShow(true),
+    onMouseLeave: () => setShow(false),
+    children: show ? (
+      <div className="tooltiptext">{text}</div>
+    ) : (
+      children.props.children
     )
   });
 };
