f357c3b7430cdf7c2577ad74c8ddb5b8f27c7afb
shubh <shubhamsharmajcc@gmail.com>
2026-02-11 21:13:06 +0530

    changes

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -3,20 +3,17 @@ import React, { useState } from "react";
 const Tooltip = ({ text, children }) => {
   const [show, setShow] = useState(false);
 
-  // Detect the tag name of the child (h2, p, etc.)
-  const ChildTag = children.type === "h2" ? "h2" : "div";
-
-  return (
-    <span
-      className="tooltip"
-      onMouseEnter={() => setShow(true)}
-      onMouseLeave={() => setShow(false)}
-    >
-      {children}
-
-      {show && <ChildTag className="tooltiptext">{text}</ChildTag>}
-    </span>
-  );
+  return React.cloneElement(children, {
+    className: `${children.props.className || ""} tooltip`,
+    onMouseEnter: () => setShow(true),
+    onMouseLeave: () => setShow(false),
+    children: (
+      <>
+        {children.props.children}
+        {show && <div className="tooltiptext">{text}</div>}
+      </>
+    ),
+  });
 };
 
 export default Tooltip;
